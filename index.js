f8f1cd868c8be6b05e44f461e179acd442542dbb
CarlosGT7274 <carlosbt7274@gmail.com>
2022-12-17 12:33:03 -0600

    commit1

---
+++ index.js
@@ -0,0 +1,13 @@
+const contenedor = document.querySelector("body")
+
+function agregar(){
+    contenedor.innerHTML += `<h1 id="encabezado">Encabezado.html</h1>`
+}
+
+function parrafo(){
+    contenedor.innerHTML += `<p id="parrafo">Lorem ipsum aondwienfiwe</p>`
+}
+
+function alerta(){
+    alert("presionaste alerta")
+}
